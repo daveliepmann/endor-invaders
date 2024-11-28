0993691affc08714ead74b2500b41ce10aef4562
Dave Liepmann <dave.liepmann@gmail.com>
2024-11-28 11:42:45 +0100

    1st solve the easiest possible sub-problem

--- src/space_defense_force/radar.clj
+++ src/space_defense_force/radar.clj
@@ -1,5 +1,7 @@
 (ns space-defense-force.radar
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
 
 (def known-invaders
   #{(slurp (io/resource "samples/invader1"))
@@ -7,5 +9,30 @@
 
 
 (def radar-sample
-  (slurp (io/resource "samples/radar1")))
+  (str/split-lines (slurp (io/resource "samples/radar1"))))
 
+
+
+
+(comment ;;;; Let's solve an easier problem first: exact match, one line at a time
+  
+  (def dev:samples
+    (into #{}
+          (map (comp first str/split-lines))
+          known-invaders))
+
+  (->> known-invaders
+       (mapcat str/split-lines)
+       (map count)
+       distinct)
+  ;; => (11 8)
+
+  ;; minimum viable detection (exact match 1 at a time)
+
+  (let [invdr (first dev:samples) ; "---oo---"
+        len (count invdr)]
+    (some #{invdr}
+          (map #(apply str %)
+               (partition len 1 (nth radar-sample 4)))))
+
+  )
